e7537849ed2ec4e719bad33e8a9484a84ae6693e
Thanh Lan <emeraldlan1105@gmail.com>
2023-11-03 02:55:56 +0700

    remove split

--- src/app/workspace/page.js
+++ src/app/workspace/page.js
@@ -3,14 +3,12 @@ import { useState } from 'react'
 import Description from '@/components/Workspace/Description'
 import HTMLEditor from '@/components/Workspace/HTMLEditor'
 import Preview from '@/components/Workspace/Preview'
-import Split from 'react-split'
 
 const Workspace = () => {
 	const [value, setValue] = useState('')
 
 	return (
 		<main className='h-full flex flex-row justify-center align-middle'>
-			<Split className='split h-full w-full'>
 				<Description />
 				<div className='w-[50%] bg-slate-300'>
 					<Preview value={value} />
@@ -19,7 +17,6 @@ const Workspace = () => {
 						setValue={setValue}
 					/>
 				</div>
-			</Split>
 		</main>
 	)
 }
